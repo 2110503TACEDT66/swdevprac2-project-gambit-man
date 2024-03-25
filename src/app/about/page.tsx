bf664a51ec00ecc5665f2dd2ff5ed4ad0198ae2f
bonkizu <isukaomu@gmail.com>
2024-03-25 14:02:43 +0700

    car

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
-export default function About(){
-    return (
-        <main>
-            <div>About Us</div>
-        </main>
-    );
-}
+'use client';
+import myImage from '@/public/img/door.jpg';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const InitialTransition = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <div className="absolute inset-0 flex items-center justify-center overflow-hidden">
+      <Image
+        src={myImage}
+        alt="My Image"
+        className={`w-full h-screen transition-all duration-1000 ${
+          isLoaded ? 'translate-y-[-100%]' : 'translate-y-0'
+        }`}
+        onClick={() => setIsLoaded(!isLoaded)}
+      />
+    </div>
+  );
+};
+
+export default InitialTransition;
