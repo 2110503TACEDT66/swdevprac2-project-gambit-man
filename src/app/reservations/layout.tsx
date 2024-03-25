ab3918476ee644ac092b334853aa6272093185bd
bonkizu <isukaomu@gmail.com>
2024-03-26 00:56:03 +0700

    fix

--- src/app/reservations/layout.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './reservations.module.css';
-export default function ReservationLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className={styles.sectionlayout}>{children}</div>;
-}
