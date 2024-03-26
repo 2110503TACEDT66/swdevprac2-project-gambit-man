ad663b34e722d047cb46791576396e8972818ccf
Tuckkrub00 <pantana2004@gmail.com>
2024-03-26 16:12:03 +0700

    edit getcar

--- src/libs/getCar.tsx
+++
@@ -1,8 +0,0 @@
-export default async function getCar(id:string){
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/cars/${id}`)
-    if(!response.ok) {
-        throw new Error("Failed to fetch cars")
-    }
-
-    return await response.json()
-}
