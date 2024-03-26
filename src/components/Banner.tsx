f722b1ea6fe1aa0004516dfd7e374892d29275d8
bonkizu <isukaomu@gmail.com>
2024-03-26 20:19:13 +0700

    update banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,37 +6,15 @@ export default function Banner() {
   const router = useRouter();
 
   const { data: session } = useSession();
-  console.log(session?.user.token);
   return (
-    <div>
-      <div className="carousel w-full">
-
-  <div id="item1" className="carousel-item w-full">
-    <img src="/img/a1.jpg" className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src="/img/a2.jpg" className="w-full" />
-  </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src="/img/a3.jpg" className="w-full" />
-  </div> 
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-</div>
-      {/* <button
-        className="bg-white text-cyan-600 border border-cyan-600
-            font-semibold py-2 px-2 m-2 rounded z-30 top-50px right-10 fixed
-            hover:bg-cyan-600 hover:text-white hover:border-transparent "
-        onClick={(e) => {
-          e.stopPropagation();
-          router.push('/car');
-        }}
-      >
-        Select Your Travel Parner NOW
-      </button> */}
+    <div className="diff aspect-[16/9]">
+      <div className="diff-item-1">
+        <img alt="cover" src="/img/a1.jpg" />
+      </div>
+      <div className="diff-item-2">
+        <img alt="daisy" src="/img/a2.jpg" />
+      </div>
+      <div className="diff-resizer"></div>
     </div>
   );
 }
