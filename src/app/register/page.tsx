b8c6be7bdd4d2a4175bc6d610c1cba74bf63476b
bonkizu <isukaomu@gmail.com>
2024-03-26 15:44:43 +0700

    update register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@
 import { UserRegister } from '@/interfaces';
 import userRegister from '@/src/libs/userRegister';
 import { redirect } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 export default function Register() {
   const [name, setName] = useState<string | null>(null);
@@ -10,8 +10,15 @@ export default function Register() {
   const [telephone, setTelephone] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
   const router = useRouter();
+  const [nameValid, setNameValid] = useState(false);
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [telephoneValid, setTelephoneValid] = useState(false);
   const addUser = async () => {
     if (!name || !email || !telephone || !password) {
+      return;
+    }
+    if (!nameValid || !emailValid || !passwordValid || !telephoneValid) {
       return;
     }
     try {
@@ -22,21 +29,51 @@ export default function Register() {
         userPassword: password,
       };
       const newUser = await userRegister(user);
-      router.push('/api/auth/signin');
+
+      router.replace('/api/auth/signin');
       router.refresh();
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    if (!name || !/^[a-zA-Z]*$/.test(name)) {
+      setNameValid(false);
+    } else {
+      setNameValid(true);
+    }
+
+    if (
+      !email ||
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    ) {
+      setEmailValid(false);
+    } else {
+      setEmailValid(true);
+    }
+
+    if (!password || password?.length < 8) {
+      setPasswordValid(false);
+    } else {
+      setPasswordValid(true);
+    }
+
+    if (!telephone || !/^0\d{2}-\d{3}-\d{4}$/.test(telephone)) {
+      setTelephoneValid(false);
+    } else {
+      setTelephoneValid(true);
+    }
+  }, [name, email, password, telephone]);
+
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 gap-y-3 mt-10">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Register</h1>
+          <h1 className="text-5xl font-bold m-10">Register</h1>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form className="card-body gap-y-4">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -49,6 +86,44 @@ export default function Register() {
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
+            {nameValid ? (
+              <div role="alert" className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your name is valid</span>
+              </div>
+            ) : (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>
+                  Your name is invalid <br />
+                  Name contains only letters
+                </span>
+              </div>
+            )}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -61,6 +136,41 @@ export default function Register() {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+            {emailValid ? (
+              <div role="alert" className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your Email is valid</span>
+              </div>
+            ) : (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your Email is invalid</span>
+              </div>
+            )}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
@@ -74,6 +184,44 @@ export default function Register() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
+            {passwordValid ? (
+              <div role="alert" className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your Password is valid</span>
+              </div>
+            ) : (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>
+                  Your Password is invalid <br />
+                  Password must be at least 8 characters
+                </span>
+              </div>
+            )}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Tel</span>
@@ -87,16 +235,54 @@ export default function Register() {
                 pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               />
             </div>
+            {telephoneValid ? (
+              <div role="alert" className="alert alert-success">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Your Tel is valid</span>
+              </div>
+            ) : (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>
+                  Your Tel is invalid <br />
+                  Tel format must be 0xx-xxx-xxxx
+                </span>
+              </div>
+            )}
             <div className="form-control mt-6">
               <button
                 className="btn btn-primary"
                 type="submit"
-                onClick={(e: React.FormEvent) => {
+                onClick={(e) => {
                   e.preventDefault();
                   addUser();
                 }}
               >
-                Login
+                Register
               </button>
             </div>
           </form>
